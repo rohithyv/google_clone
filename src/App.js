4fe7166262a0cb462513815e4316e34fcd31103c
rohithyv <53818270+rohithyv@users.noreply.github.com>
2026-02-16 19:48:29 -0500

    fix: add BrowserRouter wrapper to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Routes } from './components/Routes';
@@ -7,9 +7,15 @@ import { Routes } from './components/Routes';
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div className={darkTheme ? 'dark' : ''}>
-        <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <div className={darkTheme ? 'dark' : ''}>
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+          <Navbar/>
+          <Routes/>
+          <Footer/>
+        </div>    
+     </div>
+    </BrowserRouter>
   );
 }
 
