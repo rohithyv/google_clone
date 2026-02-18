40d71d844cbb2079ee268a95a18e5004edc1c6d1
rohithyv <53818270+rohithyv@users.noreply.github.com>
2026-02-17 21:14:31 -0500

    update: latest Google clone changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
