f7693b5bd51ed113c4848427ad537be0448b084f
rohithyv <53818270+rohithyv@users.noreply.github.com>
2026-02-17 22:27:38 -0500

    feat: style dark theme toggle button with hover effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = ({darkTheme, setDarkTheme}) => {
                     Google 🔎
                 </p>
             </Link>
-            <button type="button" onClick={() => setDarkTheme(!darkTheme)}>
+            <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
                 {darkTheme ? 'Light' : 'Dark'}
             </button>
     </div>
